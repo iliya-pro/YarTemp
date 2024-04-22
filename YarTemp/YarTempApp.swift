81cda93368c5097c3c41510f892358fca8feb915
Iliya Prostakishin <iliya.prostakishin@gmail.com>
2024-04-22 20:20:31 +0300

    Initial Commit

---
+++ YarTemp/YarTempApp.swift
@@ -0,0 +1,17 @@
+//
+//  YarTempApp.swift
+//  YarTemp
+//
+//  Created by Iliya Prostakishin on 22.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct YarTempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
